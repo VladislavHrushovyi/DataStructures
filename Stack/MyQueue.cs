01b0d4c51adcf25c0a5af4bf81ecf2a3d48b7273
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2023-01-03 23:34:56 +0200

    Add queue by stack

---
+++ Stack/MyQueue.cs
@@ -0,0 +1,35 @@
+﻿namespace Stack;
+
+public class MyQueue<T>
+{
+    private readonly MyStack<T> _stack1 = new();
+    private readonly MyStack<T> _stack2 = new();
+
+    public void EnQueue(T data)
+    {
+        while (!_stack1.IsEmpty())
+        {
+            _stack2.Push(_stack1.Pop());
+        }
+        
+        _stack1.Push(data);
+
+        while (!_stack2.IsEmpty())
+        {
+            _stack1.Push(_stack2.Pop());
+        }   
+    }
+
+    public T DeQueue()
+    {
+        if (_stack1.IsEmpty())
+        {
+            throw new Exception("Queue is empty");
+        }
+
+        var itemObjData = _stack1.Peek();
+        _stack1.Pop();
+
+        return itemObjData;
+    }
+}
