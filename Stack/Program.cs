6e14be47e36dca659983884c8bea9cb6db266205
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2023-01-03 23:35:13 +0200

    testing code

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -17,3 +17,36 @@ stack1.Pop();
 
 stack1.Display();
 Console.WriteLine($"Top element is {stack1.Peek()}");
+
+Console.WriteLine("#################----QUEUE----####################");
+
+var q = new MyQueue<int>();
+
+q.EnQueue(1);
+q.EnQueue(2);
+q.EnQueue(3);
+
+Console.WriteLine(q.DeQueue());
+Console.WriteLine(q.DeQueue());
+Console.WriteLine(q.DeQueue());
+
+Console.WriteLine("###################################################");
+
+string brackets = "()09090()()()()()9090900)))((()))((())(())(";
+
+var stackRightBrackets = new MyStack<char>();
+var stackLeftBrackets = new MyStack<char>();
+
+foreach (var item in brackets)
+{
+    if(item == '(')
+        stackLeftBrackets.Push(item);
+    else if (item == ')')
+        stackRightBrackets.Push(item);
+}
+
+if (stackRightBrackets.Count == stackLeftBrackets.Count)
+    Console.WriteLine("Brackets valid");
+else
+    Console.WriteLine("Brackets invalid");
+
