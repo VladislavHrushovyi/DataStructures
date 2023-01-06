2e754bd42c71ecb05006e9223a3974a537532626
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2023-01-06 15:01:08 +0200

    Add recursion realization

---
+++ Recursion/RecursionMethods.cs
@@ -0,0 +1,37 @@
+﻿namespace Recursion;
+
+public class RecursionMethods
+{
+    public void PrintSequence(int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        PrintSequence(n - 1);
+
+        Console.WriteLine(n);
+    }
+
+    public int CulcFibNumber(int x)
+    {
+        if (x == 0)
+            return 0;
+        if (x == 1 || x == 2)
+        {
+            return 1;
+        }
+        
+        return CulcFibNumber(x - 1) + CulcFibNumber(x - 2);
+    }
+
+    public int Factorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+
+        return n * Factorial(n - 1);
+    }
+}
