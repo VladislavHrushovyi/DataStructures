8c94f610a42ab3ecb24bd9099182a248e88b2f27
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2023-02-12 15:56:43 +0200

    Implement basics hash functions

---
+++ HashFT/HashGenerator.cs
@@ -0,0 +1,68 @@
+﻿using System.Text;
+
+namespace HashFT;
+
+public static class HashGenerator
+{
+    private const int MagicCoef = 97;
+    private const double A = 0.3252342;
+
+    public static int ByDividing(int value)
+    {
+        return value % MagicCoef;
+    }
+
+    public static int ByDividing(string data)
+    {
+        var sumAscii = Encoding.ASCII.GetBytes(data).Sum(s => s);
+
+        return sumAscii % MagicCoef;
+    }
+
+    public static int ByMidSquare(int value)
+    {
+        var charsSquare = (value * value).ToString().ToCharArray();
+        if (charsSquare.Length <= 3)
+        {
+            return Int32.Parse(charsSquare[1].ToString());
+        }
+
+        var midIndex = new Index(charsSquare.Length / 2);
+        Range range = midIndex..(midIndex.Value + 2);
+        var midValue = charsSquare[range];
+        return Int32.Parse(midValue);
+    }
+
+    public static int ByMidSquare(string data)
+    {
+        var sumAscii = Encoding.ASCII.GetBytes(data).Sum(s => s);
+        
+        return ByMidSquare(sumAscii);
+    }
+
+    public static int ByDigitFolding(int value)
+    {
+        var sumOfDigitPairs = value.ToString().Chunk(2).Sum(s => Int32.Parse(s));
+
+        return sumOfDigitPairs;
+    }
+
+    public static int ByDigitFolding(string data)
+    {
+        var sumAscii = Encoding.ASCII.GetBytes(data).Sum(s => s);
+
+        return ByDigitFolding(sumAscii);
+    }
+
+    public static int ByMultiplication(int value)
+    {
+        return Convert.ToInt32(Math.Floor(MagicCoef * (value * A)));
+    }
+
+    public static int ByMultiplication(string data)
+    {
+        var sumAscii = Encoding.ASCII.GetBytes(data).Sum(s => s);
+
+        return ByMultiplication(sumAscii);
+    }
+}
---
+++ HashFT/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using HashFT;
+
+int value = 365;
+string data = "Hello wowrld";
+Console.WriteLine("____________DIVIDING METHOD____________");
+Console.WriteLine($"Value {value} have hash {HashGenerator.ByDividing(value)}");
+Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByDividing(data)}");
+Console.WriteLine("____________END_____________");
+
+Console.WriteLine("_____________MID-SQUARE METHOD____________");
+Console.WriteLine($"Value {value} have hash {HashGenerator.ByMidSquare(value)}");
+Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByMidSquare(data)}");
+Console.WriteLine("____________END_____________");
+
+Console.WriteLine("_____________DIGIT FOLDING METHOD____________");
+Console.WriteLine($"Value {value} have hash {HashGenerator.ByDigitFolding(value)}");
+Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByDigitFolding(data)}");
+Console.WriteLine("____________END_____________");
+
+Console.WriteLine("_____________DIGIT FOLDING METHOD____________");
+Console.WriteLine($"Value {value} have hash {HashGenerator.ByMultiplication(value)}");
+Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByMultiplication(data)}");
+Console.WriteLine("____________END_____________");
