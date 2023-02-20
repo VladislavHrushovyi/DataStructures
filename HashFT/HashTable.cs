d8633a24686fd0bedf7de804d85982568cd9b7fc
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2023-02-20 17:29:41 +0200

    Implement simple hash table

---
+++ HashFT/HashTable.cs
@@ -0,0 +1,48 @@
+﻿using Tree;
+
+namespace HashFT;
+
+public class HashTable<TValue> where TValue : IComparable
+{
+    private AvlTree<TValue>[] Table = new AvlTree<TValue>[100];
+
+    public void Add(TValue item)
+    {
+        var hashValue = HashGenerator.ByDigitFolding(item.ToString());
+        if (Table[hashValue] == null)
+        {
+            Table[hashValue] = new AvlTree<TValue>();
+        }
+        Table[hashValue].Insert(item);
+    }
+
+    public void Remove(TValue val)
+    {
+        var hasValue = HashGenerator.ByDigitFolding(val.ToString());
+        Table[hasValue] = null;
+    }
+
+    public IComparable Find(TValue key)
+    {
+        var hash = HashGenerator.ByDigitFolding(key.ToString());
+        if (Table[hash] != null)
+        {
+            return Table[hash].Find(key).Data;
+        }
+
+        return null;
+    }
+
+    public void Display()
+    {
+        for(int i = 0; i < Table.Length; i++)
+        {
+            if (Table[i] != null)
+            {
+                Console.WriteLine($"Index {i}");
+                Table[i].TraverseInOrder();
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- HashFT/Program.cs
+++ HashFT/Program.cs
@@ -1,24 +1,42 @@
 ﻿
 using HashFT;
 
-int value = 365;
-string data = "Hello wowrld";
-Console.WriteLine("____________DIVIDING METHOD____________");
-Console.WriteLine($"Value {value} have hash {HashGenerator.ByDividing(value)}");
-Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByDividing(data)}");
-Console.WriteLine("____________END_____________");
+// int value = 555;
+// string data = "Hello wowrld";
+// Console.WriteLine("____________DIVIDING METHOD____________");
+// Console.WriteLine($"Value {value} have hash {HashGenerator.ByDividing(value)}");
+// Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByDividing(data)}");
+// Console.WriteLine("____________END_____________");
+//
+// Console.WriteLine("_____________MID-SQUARE METHOD____________");
+// Console.WriteLine($"Value {value} have hash {HashGenerator.ByMidSquare(value)}");
+// Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByMidSquare(data)}");
+// Console.WriteLine("____________END_____________");
+//
+// Console.WriteLine("_____________DIGIT FOLDING METHOD____________");
+// Console.WriteLine($"Value {value} have hash {HashGenerator.ByDigitFolding(value)}");
+// Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByDigitFolding(data)}");
+// Console.WriteLine("____________END_____________");
+//
+// Console.WriteLine("_____________DIGIT FOLDING METHOD____________");
+// Console.WriteLine($"Value {value} have hash {HashGenerator.ByMultiplication(value)}");
+// Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByMultiplication(data)}");
+// Console.WriteLine("____________END_____________");
 
-Console.WriteLine("_____________MID-SQUARE METHOD____________");
-Console.WriteLine($"Value {value} have hash {HashGenerator.ByMidSquare(value)}");
-Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByMidSquare(data)}");
-Console.WriteLine("____________END_____________");
+var hashTable = new HashTable<string>();
+var values = Enumerable.Range(1, 100)
+    .Select(_ => new Random().Next(1, 1000));
 
-Console.WriteLine("_____________DIGIT FOLDING METHOD____________");
-Console.WriteLine($"Value {value} have hash {HashGenerator.ByDigitFolding(value)}");
-Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByDigitFolding(data)}");
-Console.WriteLine("____________END_____________");
+foreach (var value in values)
+{
+    hashTable.Add(value.ToString());    
+}
 
-Console.WriteLine("_____________DIGIT FOLDING METHOD____________");
-Console.WriteLine($"Value {value} have hash {HashGenerator.ByMultiplication(value)}");
-Console.WriteLine($"Value \"{data}\" have hash {HashGenerator.ByMultiplication(data)}");
-Console.WriteLine("____________END_____________");
+hashTable.Display();
+
+Console.WriteLine("Enter key to find value");
+var key = Convert.ToInt32(Console.ReadLine());
+
+var findValues = hashTable.Find(key.ToString());
+
+Console.WriteLine($"Findind value {findValues}");
