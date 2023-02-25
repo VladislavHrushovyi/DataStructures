2ed394a69d533daeb895b002f81156fe317183e0
Vlad Hrushovyi <vlad.grush.99@gmail.com>
2023-02-25 20:35:14 +0200

    Uncomment block

--- Tree/Program.cs
+++ Tree/Program.cs
@@ -37,38 +37,38 @@
 // Console.WriteLine($"Tree node by index {key}");
 // Console.WriteLine(myTree[key].Data);
 
-var rbTree = new RbTree<int>();
-var values = Enumerable.Range(1,15).Select(_ => new Random().Next(1, 1000));
-
-for (int i = 0; i < 20; i++)
-{
-    rbTree.Insert(new Random().Next(100));
-}
-
-foreach (var v in values)
-{
-    rbTree.Insert(v);
-}
-
-rbTree.TraverseInOrder();
-
-Console.WriteLine("Enter the delete value");
-var value = Int32.Parse(Console.ReadLine());
-rbTree.RemoveNode(value);
-rbTree.TraverseInOrder();
-Console.WriteLine($"Depth is {rbTree.GetTreeDepth()}");
-
-// var avlTree = new AvlTree<int>();
+// var rbTree = new RbTree<int>();
 // var values = Enumerable.Range(1,15).Select(_ => new Random().Next(1, 1000));
-// foreach (var v in values)
+//
+// for (int i = 0; i < 20; i++)
 // {
-//     avlTree.Insert(v);
+//     rbTree.Insert(new Random().Next(100));
 // }
 //
-// avlTree.TraverseInOrder();
+// foreach (var v in values)
+// {
+//     rbTree.Insert(v);
+// }
 //
-// Console.WriteLine("Enter the deleting value");
-// var deletedItem = Convert.ToInt32(Console.ReadLine());
+// rbTree.TraverseInOrder();
 //
-// avlTree.RemoveNode(deletedItem);
-// avlTree.TraverseInOrder();
+// Console.WriteLine("Enter the delete value");
+// var value = Int32.Parse(Console.ReadLine());
+// rbTree.RemoveNode(value);
+// rbTree.TraverseInOrder();
+// Console.WriteLine($"Depth is {rbTree.GetTreeDepth()}");
+
+var avlTree = new AvlTree<int>();
+var values = Enumerable.Range(1,15).Select(_ => new Random().Next(1, 1000));
+foreach (var v in values)
+{
+    avlTree.Insert(v);
+}
+
+avlTree.TraverseInOrder();
+
+Console.WriteLine("Enter the deleting value");
+var deletedItem = Convert.ToInt32(Console.ReadLine());
+
+avlTree.RemoveNode(deletedItem);
+avlTree.TraverseInOrder();
