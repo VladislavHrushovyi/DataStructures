681d30b96c79ab30c239e0ec722cce8cdbe4a9d2
vladg <vlad.grush.99@gmail.com>
2023-06-06 15:57:01 +0300

    create creator large file

--- SortMethods/Program.cs
+++ SortMethods/Program.cs
@@ -1,11 +1,15 @@
-﻿using SortMethods;
+﻿
 
-var sDll = new SorterDll();
-sDll.Display();
-Console.WriteLine("##########################################");
+using SortMethods.SorterLargeFile;
+
+new FileCreator(1_000_000).CreateLargeFile();
+
+// var sDll = new SorterDll();
+// sDll.Display();
+// Console.WriteLine("##########################################");
 //sDll.BubbleSort();
 //sDll.InsertionSort();
 //sDll.SelectionSort();
 //sDll.MergeSort();
-sDll.QuickSort();
-sDll.Display();
+// sDll.QuickSort();
+// sDll.Display();
---
+++ SortMethods/SorterLargeFile/FileCreator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+
+namespace SortMethods.SorterLargeFile;
+
+public class FileCreator
+{
+    private readonly long _rowCount;
+
+    public FileCreator(long rowCount)
+    {
+        _rowCount = rowCount;
+    }
+
+    public void CreateLargeFile()
+    {
+        if (File.Exists("./large_text_file.txt"))
+        {
+            File.Delete("./large_text_file.txt");
+        }
+        using var writer = File.AppendText("./large_text_file.txt");
+        for (int i = 1; i <= _rowCount; i++)
+        {
+            var rndNumber = Random.Shared.Next(0, int.MaxValue);
+            var rndChars = Enumerable.Range(10, Random.Shared.Next(10, 50))
+                .Select(_ => (char)Random.Shared.Next('A', 'Z' + 1));
+            var row = rndNumber + " " + string.Join("", rndChars) + "\n";
+
+            writer.Write(row);
+
+            if (i % (_rowCount / 10) == 0 && i != 0)
+            {
+                Console.WriteLine(i + " lines was created");
+            }
+        }
+    }
+}
